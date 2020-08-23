3a10cafe5f78bbcdc1adbb1353476ae60de96953
incidrthreat <incidrthreat@gmail.com>
2020-08-23 16:14:58 -0500

    updated grpc services and added main func

--- cmd/user-svc/user-svc.go
+++ cmd/user-svc/user-svc.go
@@ -1,21 +1,38 @@
 package main
 
 import (
+	"context"
+	"log"
+	"net"
+
 	pb "github.com/golang-friends/slack-clone/protos"
+	"google.golang.org/grpc"
 )
 
-type authServer {
+type authServer struct {
 }
 
-func (as *authServer) Login(ctx context.Context, in *pb.LoginRequest) (*pb.AuthResponse, error){
-	return pb.AuthResponse{}, nil
+func (as *authServer) Login(ctx context.Context, in *pb.LoginRequest) (*pb.AuthResponse, error) {
+	return &pb.AuthResponse{}, nil
 }
 func (as *authServer) Register(ctx context.Context, in *pb.RegisterRequest) (*pb.AuthResponse, error) {
-	return pb.AuthResponse{}, nil
+	return &pb.AuthResponse{}, nil
 }
 func (as *authServer) UsernameUsed(ctx context.Context, in *pb.UsernameUsedRequest) (*pb.UsedResponse, error) {
-	return pb.UsedResponse{}, nil
+	return &pb.UsedResponse{}, nil
 }
-func (as *authServer) EmailUsed(ctx context.Context, in *pb.EmailUsedRequest) (*pbUsedResponse, error) {
-	return pb.UsedResponse{}, nil	
+func (as *authServer) EmailUsed(ctx context.Context, in *pb.EmailUsedRequest) (*pb.UsedResponse, error) {
+	return &pb.UsedResponse{}, nil
+}
+
+func main() {
+	server := grpc.NewServer()
+	pb.RegisterAuthServiceServer(server, &authServer{})
+
+	listener, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatal("Error creating listener: ", err.Error())
+	}
+
+	server.Serve(listener)
 }
