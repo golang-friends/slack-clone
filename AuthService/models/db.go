70931bcdbc4c0209d4f61ef4b3c68e50d92f35af
Mo Kweon <kkweon@google.com>
2020-08-25 21:19:11 -0700

    Add docker-compose

--- AuthService/models/db.go
+++ AuthService/models/db.go
@@ -34,7 +34,7 @@ func ConnectToDB(conf configs.Configuration) {
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(fmt.Sprintf("mongodb+srv://%s:%s@%s", db.DBUser, db.DBPassword, db.DBURI)))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(fmt.Sprintf("mongodb://%s:%s@%s", db.DBUser, db.DBPassword, db.DBURI)))
 	if err != nil {
 		log.Fatal("Error connecting to DB: ", err.Error())
 	}
